9da21f40239c5fd6ddf3fc4c837b23ad14681879
Bianca Santos <biancafs23@outlook.com>
2023-05-03 09:18:54 -0300

    Aula 9 - Exercício 1
    Signed-off-by: Bianca Santos <biancafs23@outlook.com>

--- methods.py
+++ methods.py
@@ -5,3 +5,19 @@ def area_of_rectangle(width, height):
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+def soma(value1, value2):
+    somar = value1 + value2
+    return somar
+
+def sub(value1, value2):
+    subtrair = value1 - value2
+    return subtrair
+
+def mult(value1, value2):
+    multiplicar = value1 * value2
+    return multiplicar
+
+def div(value1, value2):
+    dividir = value1/value2
+    return dividir
--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,47 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+    
+def teste_soma():
+    # given a value 1 of 7 and a value 2 of 8
+    value1 = 7
+    value2 = 8
+    
+    # when we calculate the sum
+    output = methods.soma(value1, value2)
+    
+    # then the result shoud be 15
+    assert output == 15
+    
+def teste_subtracao():
+    # given a value 1 of 9 and a value 2 of 6
+    value1 = 9
+    value2 = 6
+    
+    # when we calculate the sub
+    output = methods.sub(value1,value2)
+    
+    # then the result should be 3
+    assert output == 3
+    
+def teste_multiplicacao():
+    # given a value 1 of 4 and a value 2 of 2
+    value1 = 4
+    value2 = 2
+    
+    # when we calculate the mult
+    output = methods.mult(value1,value2)
+    
+    # then the result should be 8
+    assert output == 8
+    
+def teste_divisao():
+    # given a value 1 of 14 and a value 2 of 7
+    value1 = 14
+    value2 = 7
+    
+    # when we calculate the div
+    output = methods.div(value1,value2)
+    
+    # then the result should be 2
+    assert output == 2
